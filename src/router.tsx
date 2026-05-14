fb34f161e18c081c0c0fc54e095ff571429a8a13
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-05-14 16:01:26 +0530

    add persist auth on refresh

--- src/router.tsx
+++ src/router.tsx
@@ -3,25 +3,32 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/login/login";
 import NonAuth from "./Layouts/NonAuth";
 import { Dashboard } from "./Layouts/Dashboard";
+import { Root } from "./Layouts/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Root />,
     children: [
       {
-        path: "",
-        element: <HomePage />,
+        path: "/",
+        element: <Dashboard />,
+        children: [
+          {
+            path: "",
+            element: <HomePage />,
+          },
+        ],
       },
-    ],
-  },
-  {
-    path: "/auth",
-    element: <NonAuth />,
-    children: [
       {
-        path: "login",
-        element: <LoginPage />,
+        path: "/auth",
+        element: <NonAuth />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+        ],
       },
     ],
   },
