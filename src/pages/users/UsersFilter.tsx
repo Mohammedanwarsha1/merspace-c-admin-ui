e196313f98392326549de4259edbce8e31fc7ac6
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-05-17 17:55:53 +0530

    add user filter intractivity

--- src/pages/users/UsersFilter.tsx
+++ src/pages/users/UsersFilter.tsx
@@ -1,20 +1,30 @@
 import { Button, Card, Col, Input, Row, Select } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
-export const UsersFilter = () => {
+type UserFilterProps = {
+  onFilterChange: (filterName: string, filterValue: string) => void;
+};
+
+export const UsersFilter = ({ onFilterChange }: UserFilterProps) => {
   return (
     <Card>
       <Row justify="space-between">
         <Col span={16}>
           <Row gutter={20}>
             <Col span={8}>
-              <Input.Search placeholder="Search" />
+              <Input.Search
+                placeholder="Search"
+                onChange={(e) => onFilterChange("searchFilter", e.target.value)}
+              />
             </Col>
             <Col span={8}>
               <Select
                 style={{ width: "100%" }}
                 allowClear={true}
                 placeholder="Select role"
+                onChange={(selectedItem) =>
+                  onFilterChange("roleFilter", selectedItem)
+                }
               >
                 <Select.Option value="admin">Admin</Select.Option>
                 <Select.Option value="manager">Manager</Select.Option>
@@ -22,7 +32,13 @@ export const UsersFilter = () => {
               </Select>
             </Col>
             <Col span={8}>
-              <Select style={{ width: "100%" }} placeholder="Status">
+              <Select
+                style={{ width: "100%" }}
+                placeholder="Status"
+                onChange={(selectedItem) =>
+                  onFilterChange("statusFilter", selectedItem)
+                }
+              >
                 <Select.Option value="ban">Ban</Select.Option>
                 <Select.Option value="active">Active</Select.Option>
               </Select>
