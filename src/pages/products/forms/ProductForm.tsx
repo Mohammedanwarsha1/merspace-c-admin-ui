811ad4062bb9ae9a36aeb3e3278deb2297de0712
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-06-06 22:04:41 +0530

    add user role check for tenant select box

--- src/pages/products/forms/ProductForm.tsx
+++ src/pages/products/forms/ProductForm.tsx
@@ -14,8 +14,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getCategories, getTenants } from "../../../http/api";
 import Pricing from "./Pricing";
 import Attributes from "./Attributes";
+import { useAuthStore } from "../../../store";
 
 const ProductForm = () => {
+  const { user } = useAuthStore();
   const selectedCategory = Form.useWatch("categoryId");
   console.log(selectedCategory);
   const { data: categories } = useQuery({
@@ -107,37 +109,38 @@ const ProductForm = () => {
               <Col span={12}></Col>
             </Row>
           </Card>
-
-          <Card title="Tenant info" bordered={false}>
-            <Row gutter={24}>
-              <Col span={24}>
-                <Form.Item
-                  label="Restaurant"
-                  name="tenantId"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Restaurant is required",
-                    },
-                  ]}
-                >
-                  <Select
-                    size="large"
-                    style={{ width: "100%" }}
-                    allowClear={true}
-                    onChange={() => {}}
-                    placeholder="Select restaurant"
+          {user?.role !== "manager" && (
+            <Card title="Tenant info" bordered={false}>
+              <Row gutter={24}>
+                <Col span={24}>
+                  <Form.Item
+                    label="Restaurant"
+                    name="tenantId"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Restaurant is required",
+                      },
+                    ]}
                   >
-                    {restaurants?.data.data.map((tenant: Tenant) => (
-                      <Select.Option value={tenant.id} key={tenant.id}>
-                        {tenant.name}
-                      </Select.Option>
-                    ))}
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Card>
+                    <Select
+                      size="large"
+                      style={{ width: "100%" }}
+                      allowClear={true}
+                      onChange={() => {}}
+                      placeholder="Select restaurant"
+                    >
+                      {restaurants?.data.data.map((tenant: Tenant) => (
+                        <Select.Option value={tenant.id} key={tenant.id}>
+                          {tenant.name}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Card>
+          )}
 
           {selectedCategory && <Pricing selectedCategory={selectedCategory} />}
           {selectedCategory && (
